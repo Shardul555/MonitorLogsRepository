1447e3503775552edbe2df16df9faeb3236d3228
Shardul555 <semwalshardul@gmail.com>
2025-01-14 14:21:00 +0530

    Changes for correct output for multiple connections

--- src/main/java/com/example/MonitorLogsRepository/WebSocketMessageService.java
+++ src/main/java/com/example/MonitorLogsRepository/WebSocketMessageService.java
@@ -10,31 +10,37 @@ import org.springframework.web.socket.WebSocketSession;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Stack;
 
 @Service
 public class WebSocketMessageService{
+
     @Value("src/main/resources/templates/RandomTextFile")
     public File file;
     public String currentLogs;
     public long lastModified=0L;
     public long prevLength=0L;
-    @Scheduled(fixedRate = 1000)
-    public void run() {
-            for (WebSocketSession session : WebSocketHandler.getSessions()) {
-                if (session.isOpen()) {
-                    long currLastModified = file.lastModified();
-                    long currLength = file.length();
-                    if (lastModified!=currLastModified) {
-                        try {
-                            ReverseInputStream reverseInputStream = new ReverseInputStream(file, prevLength);
-                            lastModified = currLastModified;
-                            prevLength = currLength;
-                            currentLogs = reverseInputStream.pushLastLines();
+    public static Deque<String> lastLogs = new ArrayDeque<>();
+
+    @Scheduled(fixedDelay = 1000)
+    public void run() throws IOException {
+            long currLastModified = file.lastModified();
+            long currLength = file.length();
+            if (lastModified!=currLastModified && !WebSocketHandler.getSessions().isEmpty()) {
+                try {
+                    ReverseInputStream reverseInputStream = new ReverseInputStream(file, prevLength);
+                    lastModified = currLastModified;
+                    currentLogs = reverseInputStream.pushLastLines();
+                    prevLength = currLength;
+                    for (WebSocketSession session : WebSocketHandler.getSessions()) {
+                        if (session.isOpen()) {
                             session.sendMessage(new TextMessage(currentLogs));
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
                         }
                     }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
                 }
             }
     }
